52617f4329160262d24ec0200b65fc3890679135
Avisa Poshtkouhi <aposhtkouhi@gmail.com>
2020-07-26 11:52:44 -0700

    Set Annotation and centerPoint Annotation

--- MapMyPints/MapMyPints/ViewController.m
+++ MapMyPints/MapMyPints/ViewController.m
@@ -32,12 +32,26 @@
 - (IBAction)gradientTapped:(id)sender {
 }
 
+- (void) centerMap:(MKPointAnnotation *)centerPoint {
+    [self.mapView setCenterCoordinate:centerPoint.coordinate];
+}
+
 - (void) addAnnotation {
+    
     self.luciAnno = [[MKPointAnnotation alloc] init];
-    self.luciAnno.coordinate = CLLocationCoordinate2DMake(33.6432094, -117.843995);
+    self.luciAnno.coordinate = CLLocationCoordinate2DMake(33.6432094, -117.8439953);
+    self.luciAnno.title = @"Laboratory for ubiqoutose Computing and Interaction";
+    
     
     self.wiclAnno = [[MKPointAnnotation alloc] init];
+    self.wiclAnno.coordinate = CLLocationCoordinate2DMake(34.448795, -119.6646337);
+    self.wiclAnno.title = @"Westmont Inspired Computing Lab";
+    
     self.gradientAnno = [[MKPointAnnotation alloc] init];
+    self.gradientAnno.coordinate = CLLocationCoordinate2DMake(40.677623, -73.993583);
+    self.gradientAnno.title = @"Gradient LLC";
+    
+    [self.mapView addAnnotations:@[self.luciAnno, self.wiclAnno, self.gradientAnno]];
 }
 
 @end
